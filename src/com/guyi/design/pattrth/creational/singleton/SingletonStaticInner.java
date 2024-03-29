91a6c1042fd87637f1c50274d4cf2b48619f45e9
guyi <1802366955@qq.com>
2024-03-29 22:50:02 +0800

    静态内部类单例模式

---
+++ src/com/guyi/design/pattrth/creational/singleton/SingletonStaticInner.java
@@ -0,0 +1,37 @@
+package com.guyi.design.pattrth.creational.singleton;
+
+/**
+ * 单例模式 - 静态内部类
+ *
+ * @author 孤诣
+ */
+public class SingletonStaticInner {
+    private final String id;
+
+    private SingletonStaticInner() {
+        this.id = "This singleton static inner.";
+    }
+
+    /**
+     * 静态内部类，用于持有 SingletonStaticInner 的实例
+     */
+    private static class Holder {
+        private static final SingletonStaticInner INSTANCE = new SingletonStaticInner();
+    }
+
+    @Override
+    public String toString() {
+        return "SingletonStaticInner{" + "id='" + id + '\'' + '}';
+    }
+
+    /**
+     * 全局访问点
+     */
+    public static SingletonStaticInner getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getInstance());
+    }
+}
