9a0562296ceda6fe286f430e84004400d9023d6d
guyi <1802366955@qq.com>
2024-03-31 22:37:52 +0800

    输出信息

--- src/com/guyi/design/pattrth/creational/factory/abstract0/Client.java
+++ src/com/guyi/design/pattrth/creational/factory/abstract0/Client.java
@@ -7,6 +7,7 @@ package com.guyi.design.pattrth.creational.factory.abstract0;
  */
 public class Client {
     public static void main(String[] args) {
+        System.out.println("======= 抽象工厂模式 =======");
         AbstractFactory factoryA = new ProductFactoryA();
         factoryA.createProduct("A1").operation();
         factoryA.createProduct("A2").operation();
