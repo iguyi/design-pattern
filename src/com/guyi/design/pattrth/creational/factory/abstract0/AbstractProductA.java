4ee494c0729d65b565d858c66420f30bc8746fe0
guyi <1802366955@qq.com>
2024-03-31 22:28:55 +0800

    抽象产品

---
+++ src/com/guyi/design/pattrth/creational/factory/abstract0/AbstractProductA.java
@@ -0,0 +1,11 @@
+package com.guyi.design.pattrth.creational.factory.abstract0;
+
+import com.guyi.design.pattrth.creational.factory.common.Product;
+
+/**
+ * A 类产品
+ *
+ * @author 孤诣
+ */
+public abstract class AbstractProductA implements Product {
+}
---
+++ src/com/guyi/design/pattrth/creational/factory/abstract0/AbstractProductB.java
@@ -0,0 +1,11 @@
+package com.guyi.design.pattrth.creational.factory.abstract0;
+
+import com.guyi.design.pattrth.creational.factory.common.Product;
+
+/**
+ * B 类产品
+ *
+ * @author 孤诣
+ */
+public abstract class AbstractProductB implements Product {
+}
