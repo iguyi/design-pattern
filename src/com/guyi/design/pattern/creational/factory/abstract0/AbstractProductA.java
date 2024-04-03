3568b5c61674152ade1efcb60b051576a4c34f5c
guyi <1802366955@qq.com>
2024-04-03 21:17:36 +0800

    产品类型

---
+++ src/com/guyi/design/pattern/creational/factory/abstract0/AbstractProductA.java
@@ -0,0 +1,11 @@
+package com.guyi.design.pattern.creational.factory.abstract0;
+
+import com.guyi.design.pattern.creational.factory.common.Product;
+
+/**
+ * A 类产品
+ *
+ * @author 孤诣
+ */
+public abstract class AbstractProductA implements Product {
+}
---
+++ src/com/guyi/design/pattern/creational/factory/abstract0/AbstractProductB.java
@@ -0,0 +1,11 @@
+package com.guyi.design.pattern.creational.factory.abstract0;
+
+import com.guyi.design.pattern.creational.factory.common.Product;
+
+/**
+ * B 类产品
+ *
+ * @author 孤诣
+ */
+public abstract class AbstractProductB implements Product {
+}
