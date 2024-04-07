49e134bc8a837d3aa9689f85cd1a1148f8f11626
guyi <1802366955@qq.com>
2024-04-07 13:24:55 +0800

    测试组合模式

---
+++ src/com/guyi/design/pattern/structural/composite/Client.java
@@ -0,0 +1,31 @@
+package com.guyi.design.pattern.structural.composite;
+
+/**
+ * 测试组合模式
+ *
+ * @author 孤诣
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("========= 组合模式 =========");
+
+        // 创建大学
+        Organization university = new University("大学");
+
+        // 为大学添加学院
+        Organization college1 = new College("计算机学院");
+        Organization college2 = new College("信息工程学院");
+        university.add(college1);
+        university.add(college2);
+
+        // 为学院添加系(专业)
+        college1.add(new Department("软件工程"));
+        college1.add(new Department("计算机科学与技术"));
+        college1.add(new Department("网络工程"));
+        college2.add(new Department("信息工程"));
+        college2.add(new Department("通信工程"));
+
+        // 打印
+        university.print();
+    }
+}
