fb286044224bb2e64f303193f7c29039407c35b7
guyi <1802366955@qq.com>
2024-04-08 15:21:44 +0800

    代理模式 - 静态代理 - 接口

---
+++ src/com/guyi/design/pattern/structural/proxy/static_proxy/TeacherInterface.java
@@ -0,0 +1,13 @@
+package com.guyi.design.pattern.structural.proxy.static_proxy;
+
+/**
+ * 教师接口
+ *
+ * @author 孤诣
+ */
+public interface TeacherInterface {
+    /**
+     * 授课方法
+     */
+    void teach();
+}
