dfb5027ef92efe36a5bd2458d20f2a4aae2ce5a2
guyi <1802366955@qq.com>
2024-04-09 14:59:56 +0800

    模板方法模式 - 甜豆浆

---
+++ src/main/java/com/guyi/design/pattern/behavioral/template/SweetSoyMilk.java
@@ -0,0 +1,13 @@
+package com.guyi.design.pattern.behavioral.template;
+
+/**
+ * 甜豆浆
+ *
+ * @author 孤诣
+ */
+public class SweetSoyMilk extends BaseSoyMilk {
+    @Override
+    void add() {
+        System.out.println("加入糖");
+    }
+}
