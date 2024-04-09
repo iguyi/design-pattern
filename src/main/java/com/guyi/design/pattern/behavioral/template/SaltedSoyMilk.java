17d339fe2c111a191fc14b1b1ef9a969f32c2759
guyi <1802366955@qq.com>
2024-04-09 14:59:45 +0800

    模板方法模式 - 咸豆浆

---
+++ src/main/java/com/guyi/design/pattern/behavioral/template/SaltedSoyMilk.java
@@ -0,0 +1,13 @@
+package com.guyi.design.pattern.behavioral.template;
+
+/**
+ * 咸豆浆
+ *
+ * @author 孤诣
+ */
+public class SaltedSoyMilk extends BaseSoyMilk {
+    @Override
+    void add() {
+        System.out.println("加入盐");
+    }
+}
