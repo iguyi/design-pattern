741c1e37cfd922c47ca0fdeca40df4c60ced6d1c
guyi <1802366955@qq.com>
2024-04-09 14:21:26 +0800

    代理模式 - Cglib 代理 - 目标类

---
+++ src/main/java/com/guyi/design/pattern/structural/proxy/cglib/Teacher.java
@@ -0,0 +1,12 @@
+package com.guyi.design.pattern.structural.proxy.cglib;
+
+/**
+ * 具体教师, 目标对象
+ *
+ * @author 孤诣
+ */
+public class Teacher {
+    public void teach() {
+        System.out.println("教师正在授课...");
+    }
+}
